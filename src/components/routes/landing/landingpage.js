2f5e79c096236611607b294b5ab43105b07e4499
namiwa <khairul.nasri895@gmail.com>
2020-10-21 11:29:01 +0800

    landing: removing Link

--- src/components/routes/landing/landingpage.js
+++ src/components/routes/landing/landingpage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
