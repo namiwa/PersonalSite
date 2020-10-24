16e21da9f51d971c5f76e1e4e3716b34e91a74a8
namiwa <khairul.nasri895@gmail.com>
2020-10-24 17:25:44 +0800

    chore: removed react-scroll

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './components/app/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('khairul iman');
   expect(linkElement).toBeInTheDocument();
 });
